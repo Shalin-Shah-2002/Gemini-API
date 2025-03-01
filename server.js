760261ab07ac181bfb0f1fbe5430eded349b5093
Shalin-Shah-2002 <2002shalin@gmail.com>
2025-03-01 21:01:15 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+require('dotenv').config();
+const axios = require('axios');
+const cors = require('cors');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY
+console.log(process.env.GEMINI_API_KEY)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/generate', async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        if (!prompt) {
+            return res.status(400).json({ message: 'Prompt is required' });
+        }
+
+
+        const result = await model.generateContent(prompt);
+        console.log(result.response.text());
+        res.json({ result: result.response.text() });
+
+    } catch (error) {
+        console.error("Gemini API Error:", error.message);
+        res.status(500).json({ error: "Failed to fetch response from Gemini API" });
+    }
+});
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
